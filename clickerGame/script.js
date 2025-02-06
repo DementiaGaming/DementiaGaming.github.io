43a7b9f9ddd70470cf3dddb332771944f2f1301d
DementiaGaming <sukhrathansidhu@outlook.com>
2025-02-06 19:24:17 +0000

    capitalism time
    started shop but doesnt do anything yet

--- clickerGame/script.js
+++ clickerGame/script.js
@@ -1,8 +1,19 @@
 const canvas = document.getElementById("canvas");
 const c = canvas.getContext("2d")
 
+const scoreEl = document.getElementById("score")
+const autoClickerOwnedEl = document.getElementById("ownedAC")
+const clickMultiplierOwnedEl = document.getElementById("ownedCM")
+
+const autoClickerButtonEl = document.getElementById("autoClickerBuy")
+const clickMultiplierButtonEl = document.getElementById("clickMultiplierBuy")
+
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+let score = 0
+
+let autoClickerOwned = 0
 
 // button to click
 class Button {
@@ -20,6 +31,13 @@ class Button {
         c.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
         c.fill()
     }
+
+    checkClick(event) {
+        if (event.clientX < this.x + this.size && event.clientX > this.x - this.size)
+            if (event.clientY < this.y + this.size && event.clientY > this.y - this.size)
+                score += this.clickAmount
+                scoreEl.innerHTML = score
+    }
 }
 
 const middleX = innerWidth / 2
@@ -33,12 +51,28 @@ function init() {
 
 function animate() {
     requestAnimationFrame(animate)
-    
     button.draw()
+    //console.log(score)
 
 }
 
 
+addEventListener("click", (event) => {
+    console.log(event.clientX, event.clientY)
+    button.checkClick(event)
+})
 
+autoClickerButtonEl.addEventListener("click", () => {
+    if (score >= 50) {
+        score -= 50
+        scoreEl.innerHTML = score
+        autoClickerOwned++
+        autoClickerOwnedEl.innerHTML = `Owned: ${autoClickerOwned}`
+    }
+})
+
+clickMultiplierButtonEl.addEventListener("click", () => {
+
+})
 
 init()
