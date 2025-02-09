e27a48fd81caf758f0617162e44745c69d184b4a
DementiaGaming <sukhrathansidhu@outlook.com>
2025-02-09 16:30:17 +0000

    oD update (and touch support (if it works))

--- orbDefender/script.js
+++ orbDefender/script.js
@@ -13,11 +13,12 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 class Player {
-    constructor(x, y, radius, colour){
+    constructor(x, y, radius, colour, damage){
         this.x = x
         this.y = y
         this.radius = radius
         this.colour = colour
+        this.damage = damage
     }
 
     draw() {
@@ -39,7 +40,7 @@ class Projectile {
 
     draw() {
         c.beginPath()
-        c.arc(this.x, this.y, this.radius, 0,Math.PI * 2, false)
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         c.fillStyle = this.colour
         c.fill()
     }
@@ -108,7 +109,7 @@ class Particle {
 const x = innerWidth / 2
 const y = innerHeight / 2
 
-let player = new Player(x, y, 10, "white") 
+let player = new Player(x, y, 10, "white", 10) 
 let projectiles = []
 let enemies = []
 let particles = []
@@ -125,7 +126,9 @@ let level = 1
 let xp = 0
 let maxXP = 100
 
-let damage = 10
+let damageIncreaseInterval = 0
+
+let projectileRadius = 5
 
 function addXP(amount) {
     xp += amount;
@@ -147,13 +150,23 @@ function levelUp() {
     levelEl.textContent = level;
     maxXP += 20; // xp requirement increases
     xp = 0;
+    gsap.to(player, {
+        radius: player.radius + 5
+    })
+    if (damageIncreaseInterval >= 4) {
+        player.damage += 2;
+        projectileRadius += 2;
+        damageIncreaseInterval = 0;
+    } else {
+        damageIncreaseInterval++;
+    }
 }
 
 // testing(delete later)
 //document.addEventListener("click", () => addXP(10));
 
 function init() {
-    player = new Player(x, y, 30, "white") 
+    player = new Player(x, y, 10, "white", 10) 
     projectiles = []
     enemies = []
     particles = []
@@ -171,6 +184,7 @@ function init() {
     updateXPBar();
     levelEl.textContent = level;
     damage = 10
+    damageIncreaseInterval = 0
     timerIntervalID = setInterval(updateTimer, 10)
     bonusScoreID = setInterval(addBonusScore, 1000)
     setIntervals()
@@ -190,6 +204,154 @@ function updateTimer() {
         miliseconds += 1
     }
     timeEl.innerHTML = `${seconds}:${miliseconds}`
+}
+
+function increaseSpawnSpeed() {
+    if(spawnSpeed > 300) {
+        spawnSpeed -= 100
+        nextDifficulttyIncrease += 10000
+    } else if (spawnSpeed > 100) {
+        spawnSpeed -= 50
+        nextDifficulttyIncrease += 10000
+    }   
+    clearInterval(setIntervalID)
+    clearInterval(difficultyIntervalID)
+    setIntervals()
+}
+
+function spawnEnemy() {
+    let radius
+    if (level <= 10) {
+        radius = Math.random() * (30 - 5) + 5 
+    }
+    else if (level <= 20) {
+        radius = Math.random() * (40 - 5) + 5
+    }
+    else if (level <= 25) {
+        radius = Math.random() * (50 - 5) + 5
+    }
+    else if (level <= 30) {
+        radius = Math.random() * (60 - 5) + 5
+    }
+    else if (level <= 35) {
+        radius = Math.random() * (70 - 5) + 5
+    }
+    else {
+        radius = Math.random() * (80 - 5) + 5
+    }
+
+    let x
+    let y
+
+    if (Math.random() < 0.5){
+        x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+        y = Math.random() * canvas.height
+    }
+    else {
+        x = Math.random() * canvas.width
+        y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+    }
+
+    
+    const colour = `hsl(${Math.random() * 360}, 50%, 50%)`
+    
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+    const speed = {x:Math.cos(angle) , y: Math.sin(angle)}
+    
+    enemies.push(new Enemy(x,y,radius,colour,speed))
+}
+
+function playerDie() {
+    cancelAnimationFrame(animationID)
+    modalEl.style.display = "flex"
+    bigScoreEl.innerHTML = score
+    clearInterval(setIntervalID)
+    clearInterval(difficultyIntervalID)
+    clearInterval(timerIntervalID)
+    clearInterval(bonusScoreID)
+}
+
+let animationID
+let score = 0
+function animate() {
+    animationID = requestAnimationFrame(animate)
+
+    c.fillStyle = "rgba(0, 0, 0, 0.1)"
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    player.draw()
+
+    particles.forEach((particle, index) => {
+        if (particle.alpha <= 0) {
+            particles.splice(index, 1)
+        } else {
+            particle.update()
+        }
+        
+    })
+
+    projectiles.forEach((projectile, index) => {
+        projectile.update()
+
+        if(projectile.x + projectile.radius < 0 || projectile.x - projectile.radius > canvas.width || projectile.y + projectile.radius < 0 || projectile.y - projectile.radius > canvas.height) {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0)
+        }
+    });
+
+    enemies.forEach((enemy, index) => {
+        enemy.update()
+
+        const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+        if (distance - enemy.radius - player.radius < 1) {
+            if (player.radius > 10) {
+                enemies.splice(index, 1)
+                gsap.to(player, {
+                    radius: player.radius - 10
+                })
+            } else {
+                playerDie()
+            }
+        }
+
+        projectiles.forEach((projectile, projectileIndex) => {
+            const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+            if (distance - enemy.radius - projectile.radius < 1) {
+
+                for (let i = 0; i < enemy.radius * 2; i++) {
+                    particles.push(new Particle(
+                        projectile.x,
+                        projectile.y, 
+                        Math.random() * 2, 
+                        enemy.colour, 
+                        {x: (Math.random() - 0.5) * (Math.random() * 6), 
+                        y: (Math.random() - 0.5) * (Math.random() * 6)}))
+                }
+
+                if (enemy.radius - player.damage > 5) {
+                    score += Math.floor((100 * scoreMultiplier))
+                    scoreEl.innerHTML = score
+                    gsap.to(enemy, {
+                        radius: enemy.radius - player.damage
+                    })
+                    setTimeout(() => {
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+                    addXP(10)
+                }
+                else{
+                    setTimeout(() => {
+                        score += Math.floor((250 * scoreMultiplier))
+                        scoreEl.innerHTML = score
+                        enemies.splice(index, 1)
+                        projectiles.splice(projectileIndex, 1)
+                        addXP(20)
+                    }, 0)
+                }
+            }
+        })
+    }) 
 }
 
 function addBonusScore() {
@@ -239,134 +401,12 @@ function addBonusScore() {
             scoreMultiplier = 5.0
             addXP(600)
             break;
+        case 200:
+            score += 10000
+            scoreMultiplier = 10.0
+            addXP(1000)
+            break;
     }
-}
-
-function increaseSpawnSpeed() {
-    if(spawnSpeed > 300) {
-        spawnSpeed -= 100
-        nextDifficulttyIncrease += 10000
-    } else if (spawnSpeed > 100) {
-        spawnSpeed -= 50
-        nextDifficulttyIncrease += 10000
-    }   
-    clearInterval(setIntervalID)
-    clearInterval(difficultyIntervalID)
-    setIntervals()
-}
-
-function spawnEnemy() {
-    let radius
-    if (level <= 10) {
-        radius = Math.random() * (30 - 5) + 5 
-    }
-    else if (level <= 20) {
-        radius = Math.random() * (40 - 5) + 5
-    }
-    else if (level <= 30) {
-        radius = Math.random() * (50 - 5) + 5
-    }
-    let x
-    let y
-
-    if (Math.random() < 0.5){
-        x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
-        y = Math.random() * canvas.height
-    }
-    else {
-        x = Math.random() * canvas.width
-        y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
-    }
-
-    
-    const colour = `hsl(${Math.random() * 360}, 50%, 50%)`
-    
-    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
-    const speed = {x:Math.cos(angle) , y: Math.sin(angle)}
-    
-    enemies.push(new Enemy(x,y,radius,colour,speed))
-}
-
-let animationID
-let score = 0
-function animate() {
-    animationID = requestAnimationFrame(animate)
-
-    c.fillStyle = "rgba(0, 0, 0, 0.1)"
-    c.fillRect(0, 0, canvas.width, canvas.height);
-    player.draw()
-
-    particles.forEach((particle, index) => {
-        if (particle.alpha <= 0) {
-            particles.splice(index, 1)
-        } else {
-            particle.update()
-        }
-        
-    })
-
-    projectiles.forEach((projectile, index) => {
-        projectile.update()
-
-        if(projectile.x + projectile.radius < 0 || projectile.x - projectile.radius > canvas.width || projectile.y + projectile.radius < 0 || projectile.y - projectile.radius > canvas.height) {
-            setTimeout(() => {
-                projectiles.splice(index, 1)
-            }, 0)
-        }
-    });
-
-    enemies.forEach((enemy, index) => {
-        enemy.update()
-
-        const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y)
-
-        if (distance - enemy.radius - player.radius < 1) {
-            cancelAnimationFrame(animationID)
-            modalEl.style.display = "flex"
-            bigScoreEl.innerHTML = score
-            clearInterval(setIntervalID)
-            clearInterval(difficultyIntervalID)
-            clearInterval(timerIntervalID)
-            clearInterval(bonusScoreID)
-        }
-
-        projectiles.forEach((projectile, projectileIndex) => {
-            const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
-            if (distance - enemy.radius - projectile.radius < 1) {
-
-                for (let i = 0; i < enemy.radius * 2; i++) {
-                    particles.push(new Particle(
-                        projectile.x,
-                        projectile.y, 
-                        Math.random() * 2, 
-                        enemy.colour, 
-                        {x: (Math.random() - 0.5) * (Math.random() * 6), 
-                        y: (Math.random() - 0.5) * (Math.random() * 6)}))
-                }
-
-                if (enemy.radius - damage > 5) {
-                    score += Math.floor((100 * scoreMultiplier))
-                    scoreEl.innerHTML = score
-                    gsap.to(enemy, {
-                        radius: enemy.radius - damage
-                    })
-                    setTimeout(() => {
-                        projectiles.splice(projectileIndex, 1)
-                    }, 0)
-                    addXP(10)
-                }
-                else{
-                    setTimeout(() => {
-                        score += Math.floor((250 * scoreMultiplier))
-                        scoreEl.innerHTML = score
-                        enemies.splice(index, 1)
-                        projectiles.splice(projectileIndex, 1)
-                        addXP(20)
-                    }, 0)
-                }
-            }
-        })
-    }) 
 }
 
 addEventListener("click", (event) => {
@@ -375,7 +415,17 @@ addEventListener("click", (event) => {
         x: Math.cos(angle) * 5,
         y: Math.sin(angle) * 5
     }
-    projectiles.push(new Projectile(x, y, 5, "white", speed))
+    projectiles.push(new Projectile(x, y, projectileRadius, "white", speed))
+    console.log(angle)
+})
+
+addEventListener("touchend", (event) => {
+    const angle = Math.atan2(event.touches[0].clientY - canvas.height / 2, event.touches[0].clientX - canvas.width / 2)
+    const speed = {
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5
+    }
+    projectiles.push(new Projectile(x, y, projectileRadius, "white", speed))
     console.log(angle)
 })
 
