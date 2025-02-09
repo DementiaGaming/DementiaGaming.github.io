5de7841579604b3c4d9c3dab8d48a0dccd0bde94
DementiaGaming <sukhrathansidhu@outlook.com>
2025-02-09 17:59:12 +0000

    clicker game fixes

--- clickerGame/script.js
+++ clickerGame/script.js
@@ -118,4 +118,13 @@ ultraboostButtonEl.addEventListener("click", () => {
     }
 })
 
+let lastTouchEnd = 0;
+document.addEventListener("touchend", (event) => {
+    const now = new Date().getTime();
+    if (now - lastTouchEnd <= 300) {  
+        event.preventDefault(); 
+    }
+    lastTouchEnd = now;
+}, false);
+
 init()
